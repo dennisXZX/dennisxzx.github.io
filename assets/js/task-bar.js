5442d7a97bbcc6846bac35f14eeb78d16a9cac2a
Dennis Xiao <dennisboys@gmail.com>
2018-03-03 21:44:54 +1100

    added task bar

---
+++ assets/js/task-bar.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+  // startTime();
+  $('.start').click(function () {
+    $(this).toggleClass('startNotclicked');
+    $(this).toggleClass('startClicked');
+  });
+});
+
+//
+// function startTime() {
+//   var today = new Date();
+//   var h = today.getHours();
+//   var m = today.getMinutes();
+//   var s = today.getSeconds();
+//   // add a zero in front of numbers<10
+//   m = checkTime(m);
+//   s = checkTime(s);
+//   var hd = h;
+//   $('#clock').html((hd = 0 ? "12" : hd > 12 ? hd - 12 : hd) + ":" + m + " " + (h < 12 ? "AM" : "PM"));
+//   t = setTimeout(function () { startTime() }, 500);
+// }
+//
+// function checkTime(i) {
+//   if (i < 10) {
+//     i = "0" + i;
+//   }
+//   return i;
+// }
